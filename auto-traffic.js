22a09cd5804568f5d2fb26b261229fa169e8b849
Traffic Exchange <Cold.wallet.dev@gmail.com>
2025-06-08 23:48:22 +0200

    Update auto-traffic.js

--- auto-traffic.js
+++ auto-traffic.js
@@ -39,7 +39,7 @@ window.addEventListener('load', () => {
         });
 
         // Prevent unwanted scrolling
-        window.scrollTo(0, 0);
+        //window.scrollTo(0, 0);
       }
 
       setRandomUrlInIframes();
